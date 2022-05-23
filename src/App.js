b57c5e6a49f5f8b7ae05327d12d08a41a335c369
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-23 11:15:50 -0400

    restaurant-list-ah

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+import Menubar from './components/Menubar';
 import RestaurantList from './components/RestaurantList';
-import { Layout, Menu } from 'antd';
-import { PlusCircleOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
 import './App.css';
 
 const { Header, Content } = Layout;
 
 function App() {
   return (
+    <BrowserRouter>
     <Layout className= 'layout'>
     <Header>
-      <Menu theme="dark" mode="horizontal">
+      <Menubar />
+      {/* <Menu theme="dark" mode="horizontal">
       <Menu.Item key="add" icon={<PlusCircleOutlined style={{ fontSize: '1.8em'}} />} />
       <Menu.Item key="random" icon={<QuestionCircleOutlined style={{ fontSize: '1.8em'}} />} />
-      <Menu.Item key="user" icon={<UserOutlined style={{ fontSize: '1.8em'}} />} />
-      </Menu>
+      <Menu.Item key="user" icon={<UserOutlined style={{ fontSize: '1.8em'}} />} /> */}
+      {/* </Menu> */}
     </Header>
     <Content>
-      <RestaurantList />
+      <Routes>
+        <Route path='/random' element={<h1>Random</h1>} />
+        <Route path='/add' element={<h1>Add Restaurant</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/List' element={<RestaurantList/>} />
+      </Routes>
       </Content>
       </Layout>
+      </BrowserRouter>
   );
 }
 
