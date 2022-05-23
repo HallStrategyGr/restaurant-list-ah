7c4694c3631eb8e5534b558f4c3c098360e54c70
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-23 10:51:07 -0400

    Restaurant-List

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,33 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Row, Card } from 'antd';
+import React, { useEffect, useState } from "react";
+import { Row } from "antd";
+import RestaurantCard from "./RestaurantCard";
 
 function RestaurantList() {
   const [restaurants, setRestaurants] = useState();
   useEffect(() => {
     // fetch our API
-    fetch('https://my-first-firestore-bc.web.app/restaurants')
-      .then(response => response.json())
-      .then(data => setRestaurants(data)) // data -> restaurants
-      .catch(console.error)
-  }, [])
+    fetch("https://my-first-firestore-bc.web.app/restaurants")
+      .then((response) => response.json())
+      .then((data) => setRestaurants(data)) // data -> restaurants
+      .catch(console.error);
+  }, []);
   return (
-    <section>
-      <Row gutter={16}>
-        {!restaurants
-          ? <h2>Loading...</h2>
-          : restaurants.map(restaurant => (
-            <Col style={{ width: '300px' }} key={restaurant.id}>
-              <Card
-                title={restaurant.name}
-                hoverable>
-                <p>{restaurant.address}</p>
-              </Card>
-            </Col>
-          )
-          )}
+    <section style={{ marginTop: "60px" }}>
+      <Row>
+        {!restaurants ? (
+          <h2>Loading...</h2>
+        ) : (
+          restaurants.map((restaurant) => (
+            <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+          ))
+        )}
       </Row>
     </section>
-  )
+  );
 }
 
 export default RestaurantList;
