d9a0ff27f60bac1f9e393910d282557513666f4a
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-23 14:50:24 -0400

    restaurant-list-ah

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import Menubar from './components/Menubar';
 import RestaurantList from './components/RestaurantList';
+import RestaurantPage from './components/RestaurantPage';
+import Login from './components/Login';
 import './App.css';
 
 const { Header, Content } = Layout;
@@ -9,25 +11,21 @@ const { Header, Content } = Layout;
 function App() {
   return (
     <BrowserRouter>
-    <Layout className= 'layout'>
-    <Header>
-      <Menubar />
-      {/* <Menu theme="dark" mode="horizontal">
-      <Menu.Item key="add" icon={<PlusCircleOutlined style={{ fontSize: '1.8em'}} />} />
-      <Menu.Item key="random" icon={<QuestionCircleOutlined style={{ fontSize: '1.8em'}} />} />
-      <Menu.Item key="user" icon={<UserOutlined style={{ fontSize: '1.8em'}} />} /> */}
-      {/* </Menu> */}
-    </Header>
-    <Content>
-      <Routes>
-        <Route path='/random' element={<h1>Random</h1>} />
-        <Route path='/add' element={<h1>Add Restaurant</h1>} />
-        <Route path='/login' element={<h1>Login</h1>} />
-        <Route path='/' element={<RestaurantList/>} />
-      </Routes>
-      </Content>
+      <Layout className='layout'>
+        <Header>
+          <Menubar />
+        </Header>
+        <Content>
+          <Routes>
+            <Route path='/restaurants/:restaurantId' element={<RestaurantPage />} />
+            <Route path='/random' element={<h1>Random</h1>} />
+            <Route path='/add' element={<h1>Add Restaurant</h1>} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/' element={<RestaurantList />} />
+          </Routes>
+        </Content>
       </Layout>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
